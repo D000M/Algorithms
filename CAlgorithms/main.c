fa97d04ecadf50dfb1696b6752ea0cebd79c7d97
D000M <georgislawow@gmail.com>
2018-09-29 08:35:49 +0300

    Added some main's for c, c++ and java
    Begining of algorithms

---
+++ CAlgorithms/main.c
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: DooM
+ *
+ * Created on September 29, 2018, 8:28 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    printf("C algorithms\n");
+    
+    return (EXIT_SUCCESS);
+}
+
